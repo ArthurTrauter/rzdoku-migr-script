a764f5e54671b54a4f09f1108388331145d6e486
Arthur Trauter <arthur.trauter@mannheimer.de>
2015-10-05 14:03:19 +0200

    initial commit

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+var assert = require('assert');
+var rzdokuMigrScript = require('../lib');
+
+describe('rzdoku-migr-script file-reader', function() {
+  it('should read file out of configured directory', function() {
+    assert(false,
+      'we expected this package author to add actual unit tests.');
+  });
+});
